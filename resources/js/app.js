02def1439451bbe36ef56c16c62626c6247f34bf
Gabriele <marino.gabri97@gmail.com>
2023-04-19 01:10:39 +0100

    install vue and vue plugin for vite

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
-import './bootstrap';
+// import './bootstrap';
+
+import { createApp } from 'vue';
+import Test from "./Test.vue"
+
+createApp(Test).mount('#app');
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,44 +1,34 @@
 <!DOCTYPE html>
-    <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Netsons Test</title>
-        <!-- Fonts -->
-            <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-            @vite('resources/css/app.css') <style>
-                body {
-                    font-family: 'Nunito', sans-serif;
-                    max-width: 500px;
-                    margin: auto;
-                }
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Netsons Test</title>
 
-                ol li {
-                    padding-bottom: 5px;
-                }
+    @vite(['resource/js/app.js', 'resources/css/app.css'])
+</head>
 
-            </style>
-    </head>
+<body>
+    <h1 class="text-3xl font-bold my-5">Test del candidato</h1>
+    <p class="mb-3">Benvenuto/a nel test per la tua candidatura. </p>
+    <p class="mb-3">Cosa puoi fare con questo progetto: </p>
+    <ol class="list-disc">
+        <li>aggiungere una funzionalità "form di contatto"a questa pagina. Le richieste inviate devono essere
+            salvate in una model di nome Contact. Utilizza le migration per creare la tabella necessaria,
+        </li>
+        <li>modificare questa pagina affinché sia una landing page per un prodotto/servizio di tuo gradimento.
+            L'aggiunta di effetti di movimento alla pagina è un plus gradito,</li>
+        <li>aggiungere la funzionalità di autenticazione al sistema (utilizzando eventualmente librerie standard di
+            laravel) per fare in modo che questa pagina sia accessibile solo a utenti registrati,
+        </li>
+    </ol>
+    <p class="mb-3">Sei libero/a di scegliere uno o più di uno dei punti precedenti. </p>
+    <p class="mb-3">Puoi anche cimentarti in qualcosa di completamente diverso,
+        se lo reputi interessante. Ricorda,
+        non è importante la quantità. </p>
 
-    <body>
-        <h1 class="text-3xl font-bold mb-5">Test del candidato</h1>
-        <p>Benvenuto/a nel test per la tua candidatura. </p>
-        <p>Cosa puoi fare con questo progetto: </p>
-        <ol>
-            <li>aggiungere una funzionalità "form di contatto"a questa pagina. Le richieste inviate devono essere
-                salvate in una model di nome Contact. Utilizza le migration per creare la tabella necessaria,
-            </li>
-            <li>modificare questa pagina affinché sia una landing page per un prodotto/servizio di tuo gradimento.
-                L'aggiunta di effetti di movimento alla pagina è un plus gradito,</li>
-            <li>aggiungere la funzionalità di autenticazione al sistema (utilizzando eventualmente librerie standard di
-                laravel) per fare in modo che questa pagina sia accessibile solo a utenti registrati,
-            </li>
-        </ol>
-        <p>Sei libero/a di scegliere uno o più di uno dei punti precedenti. </p>
-        <p>Puoi anche cimentarti in qualcosa di completamente diverso,
-            se lo reputi interessante. Ricorda,
-            non è importante la quantità. </p>
-    </body>
+    <div id="app"></div>
+</body>
 
-    </html>
+</html>
