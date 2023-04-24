9baccd0d4b505781f288211fdf8257b951a0da24
Gabriele <marino.gabri97@gmail.com>
2023-04-24 21:50:00 +0100

    add spinner and improve UX in login

--- resources/js/components/LoginForm.vue
+++ resources/js/components/LoginForm.vue
@@ -1,20 +1,31 @@
 <script setup>
     import axios from 'axios';
-    import { reactive } from 'vue';
+    import { ref, reactive, onMounted } from 'vue';
+    import Spinner from './Spinner.vue';
 
     const emit = defineEmits(['loggedInEvent', 'showRegisterFormEvent', 'errorEvent'])
     
+    onMounted(() => {
+        // clear error messages when mounted
+        emit('errorEvent', '');
+    });
+
     const login = reactive({
         email: '',
         password: ''
     });
 
+    const showLoadingSpinner = ref(false);
+    const opacityClass = ref('');
+    
     const handleLogin = function () {
+        // show spinner and disable form
+        toggleSpinner();
+
         axios.post(
             '/api/user/login', {
                 email: login.email,
-                password: login.password,
-                validateStatus: status => status < 400
+                password: login.password
             }
         ).then ((res) => {
             emit('loggedInEvent', {
@@ -22,35 +33,51 @@
                 expiryDate: res.data.data.accessToken.expires_at
             })
         }).catch (err => {
+            if (err.response != undefined) {
+                emit('errorEvent', err.response.data.error_message);
+                return;
+            }
             emit('errorEvent', err.message);
-        })
+        }).finally(() => {
+            toggleSpinner();
+        });
     };
+
+    const toggleSpinner = function () {
+        showLoadingSpinner.value = !showLoadingSpinner.value;
+        opacityClass.value = opacityClass.value === '' ? 'opacity-50' : '';
+    }
 </script>
 
 <template>
     <div class="mt-10">
 
-        <div id="login_form_container" class="flex flex-col p-5">
+        <div id="login_form_container" class="flex flex-col p-5 relative" :class="opacityClass">
+
+            <Spinner v-if="showLoadingSpinner"/>
 
             <h1 class="text-3xl">Login</h1>
 
-            <form action="#" id="login_form" @submit.prevent="handleLogin">
+            <form action="#" id="login_form" @submit.prevent="handleLogin" :disabled="showLoadingSpinner">
                 <!-- Email -->
                 <div class="flex flex-col my-3" id="email_container">
                     <label for="login_form_email" class="font-medium">Email</label>
-                    <input type="email" v-model="login.email" id="login_form_email" class="border-gray-300 border-2 rounded-xl" required>
+                    <input type="email" v-model="login.email" id="login_form_email" class="border-gray-300 border-2 rounded-xl" 
+                           :disabled="showLoadingSpinner" required>
                 </div>
 
                 <!-- Password -->
                 <div class="flex flex-col my-3" id="password_container">
                     <label for="login_form_password" class="font-medium">Password</label>
-                    <input type="password" v-model="login.password" id="login_form_password" minlength="8" class="border-gray-300 border-2 rounded-xl" required>
+                    <input type="password" v-model="login.password" id="login_form_password" minlength="8" class="border-gray-300 border-2 rounded-xl" 
+                           :disabled="showLoadingSpinner" required>
                 </div>
 
                 <!-- Button -->
                 <div class="flex flex-col my-6 justify-center items-center" id="submit_container">
-                    <input type="submit" name="send" id="login_form_submit" value="Login" class="border-gray-300 border-2 rounded-md py-2 w-1/3 cursor-pointer">
-                    <p class="mt-5">Not registered yet? <span class="underline cursor-pointer" @click="emit('showRegisterFormEvent', true)">Register now!</span></p>
+                    <input type="submit" name="send" id="login_form_submit" value="Login" :disabled="showLoadingSpinner"
+                           class="border-gray-300 border-2 rounded-md py-2 w-1/3 cursor-pointer">
+                    <p class="mt-5">Not registered yet? <span class="underline cursor-pointer py-2" @click="!showLoadingSpinner && emit('showRegisterFormEvent', true)">Register now!</span></p>
                 </div>
             </form>
         </div>
